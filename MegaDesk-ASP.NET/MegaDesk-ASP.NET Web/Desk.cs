1386dbff0af8b1d2e8e14c65740526dd6d936d4d
Erik Martinez <ejm20658@gmail.com>
2019-09-18 16:51:53 -0600

    MegaDesk Web App

---
+++ MegaDesk-ASP.NET/MegaDesk-ASP.NET Web/Desk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_ASP.NET_Web
+{
+    public class Desk
+    {       
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+        public int Drawers { get; set; }
+
+    }
+}
